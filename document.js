6eb97c82fd5464f6b63840fcdddf2d278e245e38
Raynos <raynos2@gmail.com>
2014-04-23 00:59:42 -0700

    add an individual cache for min-document in globla

--- document.js
+++ document.js
@@ -1,5 +1,10 @@
-if (typeof document !== "undefined") {
+if (typeof document !== 'undefined') {
     module.exports = document;
 } else {
-    module.exports = require("min-document");
+    var Individual = require('individual');
+
+    var document = Individual('__GLOBAL_DOCUMENT_CACHE@4', 
+        require('min-document'));
+
+    module.exports = document;
 }
