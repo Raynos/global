02b699a3b002a110d159568e41bcd491b358810e
Raynos <raynos2@gmail.com>
2014-05-03 14:53:10 -0700

    manual global caching

--- document.js
+++ document.js
@@ -1,10 +1,15 @@
+var topLevel = typeof global !== 'undefined' ? global :
+    typeof window !== 'undefined' ? window : {}
+var minDoc = require('min-document');
+
 if (typeof document !== 'undefined') {
     module.exports = document;
 } else {
-    var Individual = require('individual');
+    var document = topLevel['__GLOBAL_DOCUMENT_CACHE@4'];
 
-    var document = Individual('__GLOBAL_DOCUMENT_CACHE@4', 
-        require('min-document'));
+    if (!document) {
+        document = topLevel['__GLOBAL_DOCUMENT_CACHE@4'] = minDoc;
+    }
 
     module.exports = document;
 }
