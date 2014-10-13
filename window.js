7f6df564c74e5518a9b8504dafcde8dd95266e1c
Ivan Willig <iwillig@gmail.com>
2014-10-13 16:10:35 -0400

    Support the web worker context in window.js

--- window.js
+++ window.js
@@ -2,6 +2,8 @@ if (typeof window !== "undefined") {
     module.exports = window;
 } else if (typeof global !== "undefined") {
     module.exports = global;
+} else if (typeof self !== "undefined"){
+    module.exports = self;
 } else {
     module.exports = {};
 }
